6031ebb691f2f8abd94853d49cab6c1dc47acadd
Philip Sales <pj_sal87@yahoo.com>
2018-09-17 19:42:30 +0800

    Change domain name

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,5 +1,5 @@
 #clean the staging artificsts
-sudo rm -rf /docker-volumes/
+sudo rm -rf /docker-volumes-stage/
 
 #check the certificates
 sudo docker run -it --rm \
@@ -8,7 +8,7 @@ sudo docker run -it --rm \
 -v /root/kobo-docker/letsencrypt/letsencrypt-site:/data/letsencrypt \
 -v /docker-volumes-stage/var/log/letsencrypt:/var/log/letsencrypt \
 certbot/certbot \
--- staging \
+--staging \
 certificates
 
 #run the production
@@ -21,7 +21,9 @@ certbot/certbot \
 certonly --webroot \
 --email philip@alliedworld.healthcare --agree-tos --no-eff-email \
 --webroot-path=/data/letsencrypt \
--d aqm.space -d www.aqm.space -d ee.aqm.space -d kc.aqm.space -d kpi.aqm.space
+-d forms.curis.online -d kc.curis.online -d ee.curis.online
+#--webroot-path=/usr/share/nginx/html/letsencrypt-root \
+#-d aqm.space -d www.aqm.space -d ee.aqm.space -d kc.aqm.space -d kpi.aqm.space
 
 #close container
 docker-compose down
